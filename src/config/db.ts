b602572e3b8ceff1048e434998165f628474db31
Jose Ortale <jose.ortale@conjure.co.uk>
2024-09-24 17:17:18 +0100

    Change db config settings

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,12 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createPool({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: 'password',
-    database: 'house_management',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as unknown as number,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
